f6255ab2d8b7018759571d18b84fa160a2d66af8
Alexander Truong <alexander.m.truong@gmail.com>
2018-10-02 15:29:37 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,66 @@
 # your code goes here
+def begins_with_r(tools)
+new_array = tools.uniq {|x| x.start_with?("r")}
+ if new_array.size == 1 
+   return true 
+ else 
+   false
+ end
+end
+
+def contain_a(array)
+  newy = []
+  array.each do |x|
+    if x.include?("a")
+      newy << x 
+    end
+  end
+  newy
+end
+
+def first_wa(array)
+  array.each do |x|
+    y = x.to_s
+    if y.start_with?("wa")
+      return y
+    end
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if {|x| x.is_a?(String) == false}
+array
+end
+
+def count_elements(array)
+  counts = Hash.new(0)
+  array.each {|x| counts[x] += 1}
+  return_array = []
+  counts.each do |element, num|
+    element.each do |k, v|
+    return_array << {k => v, :count => num}
+  end
+end
+  return_array
+
+end
+  
+def merge_data
+
+end
+
+def find_cool
+
+end
+
+def organize_schools(schools)
+  schools.each do |school, data|
+    
+end
+
+
+
+
+
+
+  
